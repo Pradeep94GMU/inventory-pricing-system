68124b117a94e92eaf5fbc6788e4b24752136460
Pradeep Ranjan <pradeep.ranjan7837@gmail.com>
2025-01-15 13:01:34 -0500

    Initial commit

---
+++ src/main/java/com/example/inventory_pricing_system/InventoryPricingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.inventory_pricing_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryPricingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryPricingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/inventory_pricing_system/InventoryPricingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inventory_pricing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryPricingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
